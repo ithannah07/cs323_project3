0c72ff9f85f2c0346d28ec5248b7d410585f5734
ithannah <ithannah07@gmail.com>
2025-10-04 15:18:30 -0400

    finalizing

--- project3.py
+++ project3.py
@@ -9,29 +9,33 @@ def generate_value(n, min_val = 0, max_val = 100):
     return [random.randint(min_val, max_val) for _ in range(n)]
 
 def computing_time(start, end):
-    print("Computing time: ", end - start, "seconds")
+    # print("Computing time: ", end - start, "seconds")
     elasped = end - start
-    return elasped
+    return float(round(elasped, 10))
 
 def non_private(values):
     start = time.time()
-    mean = float(np.mean(values))
+    mean = float(round(np.mean(values), 3))
     end = time.time()
     elasped = computing_time(start, end)
     
     return mean, elasped
 
 
-def pillier_average(values):
+def paillier_average(values):
     start = time.time()
 
     public_key, private_key = paillier.generate_paillier_keypair()
 
-    enc_values = [public_key.excrypt(x) for x in values]
-    enc_sum = sum(enc_values)
+    enc_values = [public_key.encrypt(x) for x in values]
+
+    enc_sum = enc_values[0]
+    for i in range(1, len(enc_values)):
+        enc_sum += enc_values[i]
+    
     dec_sum = private_key.decrypt(enc_sum)
 
-    mean = dec_sum / len(values)
+    mean = round((dec_sum / len(values)), 3)
 
     end = time.time()
     elasped = computing_time(start, end)
@@ -42,28 +46,31 @@ def shamir_secret_sharing(values, n):
     start = time.time()
     # t = floor(n/2)
     t = math.floor(n/2)
-    polynomials = []
+    recovered = []
 
     # 1. Define polynomial f(x) of degree t-1 for each value
     for v in values:
         coeffs = generate_polynomial(v, t)
-        polynomials.append(coeffs)
 
         # 2. n개의 share (i, f(i)) 생성
         shares = generate_shares(coeffs, n)
-        print("shares: ", shares)
+        # print("shares: ", shares)
     
-    # 3. 임의로 t개 이상의 share 선택 (trusted party들)
+        # 3. 임의로 t개 이상의 share 선택 (trusted party들)
         selected_shares = t_shares(shares, t)
-        print("selected shares: ", selected_shares)
+        # print("selected shares: ", selected_shares)
 
-    # 4. Lagrange interpolation을 사용해 원래 값 복원
-    reconstruction_value = reconstruction(selected_shares)
+        # 4. Lagrange interpolation을 사용해 원래 값 복원
+        reconstruction_value = reconstruction(selected_shares)
+        # print("reconstruction value: ", reconstruction_value)
+        recovered.append(reconstruction_value)
 
     # 5. 모든 값 복원 후 평균 계산
-    mean = reconstruction_value / len(values)
+    mean = float(round(sum(recovered) / len(recovered), 3))
     end = time.time()
     elasped = computing_time(start, end)
+
+    return mean, elasped
 
 def generate_polynomial(value, t):
     degree = t - 1
@@ -85,11 +92,6 @@ def generate_shares(coeffs, n):
 
 def t_shares(shares, t):
     return random.sample(shares, t)
-
-def main():
-    values = generate_value(5)
-    mean = non_private(values)
-    print("average: ", mean)
 
 def reconstruction(seclected_shares):
     """
@@ -113,6 +115,26 @@ def reconstruction(seclected_shares):
         secret_i += fj * lj
 
     return int(round(secret_i))
+
+def main():
+    
+    for _ in range(5):
+        n = input("Enter the number of total parties: ")
+        n = int(n)
+        values = generate_value(n)
+        avg1, t1 = non_private(values)
+        avg2, t2 = paillier_average(values)
+        avg3, t3 = shamir_secret_sharing(values, n)
+        print("Non-private, average: ", avg1, "elapsed time: ", t1)
+        print("Paillier, average: ", avg2, "elapsed time: ", t2)
+        print("Shamir, average: ", avg3, "elapsed time: ", t3)
+        print()
+    
+
+
+
+    
+
 
 
 if __name__ == "__main__":
